d41a7cfc4750569faadf031875f7c85aa044a43d
Aaron Gullickson <aarongullickson@gmail.com>
2025-06-27 17:08:22 -0700

    Reduce chunk sizes

--- utils/functions.R
+++ utils/functions.R
@@ -150,7 +150,7 @@ resample_data <- function(ind_data, sample_design, ...) {
 
 bootstrap_model <- function(model_data_list, 
                             selected_groups,
-                            chunk_size = 50,
+                            chunk_size = 25,
                             composition_var = NULL,
                             conditional_var = NULL,
                             control_var = NULL,
@@ -170,7 +170,7 @@ bootstrap_model <- function(model_data_list,
   # now remove actual data and parallel process all of the bootstrap samples
   model_data_list <- model_data_list[-1]
   
-  # chunk results into sizes of 50 to keep down memory pressure
+  # chunk results into smaller sizes to keep down memory pressure
   chunks <- split(model_data_list, ceiling(seq_along(model_data_list) / chunk_size))
   chunked_results <- vector("list", length(chunks))
   for (i in seq_along(chunks)) {
