85d9448f29067fa7450534828188778a1defc579
Aaron Gullickson <aarongullickson@gmail.com>
2026-04-26 11:29:22 -0700

    Add rough draft of presentation

--- utils/check_packages.R
+++ utils/check_packages.R
@@ -10,7 +10,7 @@ packages = c(
   "knitr", # for processing quarto
   "readr","haven", "googlesheets4", # I/O
   "tidyverse","lubridate","broom", #tidyverse and friends
-  "ggrepel", # plotting extras
+  "ggrepel", "gganimate", # plotting extras
   "srvyr", # for adjusting by sample design
   "marginaleffects", # for computing log-odds from models
   "mice", # for imputing values
